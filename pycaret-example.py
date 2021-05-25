d0e2ed6630bb7eb63953abf469b8c2c45b930f73
Uğur Özcan <ugurozcan108@gmail.com>
2021-05-25 21:40:38 +0300

    update script and add model files

--- pycaret-example.py
+++ pycaret-example.py
@@ -18,11 +18,11 @@ def convert_date(date_string):
         print("new_date", new_date)
         return new_date
     
-def save_patients_model(patients,dates_range):
+def save_patients_model(column_name, dates_range, model_name):
     df = pd.DataFrame()
 
     df["Date"] = dates_range
-    df["patients"] = patients
+    df[model_name] = column_name
     
     # extract day, month and year from dates
     df['Day'] = [i.day for i in df['Date']]
@@ -33,15 +33,15 @@ def save_patients_model(patients,dates_range):
     df['Series'] = np.arange(1,len(df)+1)
     
     df.drop(["Date"], axis=1, inplace=True)
-    df = df[['Series','Day' ,'Year', 'Month', 'patients']] 
+    df = df[['Series','Day' ,'Year', 'Month', model_name]] 
     # check the head of the dataset
     
     train_pct_index = int(0.9 * len(df))
     train, test = df[:train_pct_index], df[train_pct_index:]
     
     # initialize setup
-    s = setup(data = train, test_data = test, target = 'patients', 
-              fold_strategy = 'timeseries', numeric_features = ['Year','Month','Series'], 
+    s = setup(data = train, test_data = test, target = model_name, 
+              fold_strategy = 'timeseries', numeric_features = ['Day','Year','Month','Series'], 
               fold = 3, session_id = 123)
     
     best = compare_models(sort = 'MAE')
@@ -55,7 +55,7 @@ def save_patients_model(patients,dates_range):
     predictions = predict_model(tuned_model, data=df)
     final_best = finalize_model(best)
     
-    save_model(final_best,'patients_final_model')
+    save_model(final_best,f'{model_name}_final_model')
     
     prediction_start_date = dates_range[-1] + datetime.timedelta(days = 1)
     prediction_end_date = prediction_start_date + datetime.timedelta(days = 6)
@@ -71,12 +71,12 @@ def save_patients_model(patients,dates_range):
     return predictions_future
 
 
-def pycaret_patients_seven_days_prediction(patients,dates_range, model):
+def pycaret_patients_seven_days_prediction(patients,dates_range, model, model_name):
     
     df = pd.DataFrame()
     
     df["Date"] = dates_range
-    df["patients"] = patients
+    df[model_name] = patients
     
     # extract day, month and year from dates
     df['Day'] = [i.day for i in df['Date']]
@@ -87,7 +87,7 @@ def pycaret_patients_seven_days_prediction(patients,dates_range, model):
     df['Series'] = np.arange(1,len(df)+1)
     
     df.drop(["Date"], axis=1, inplace=True)
-    df = df[['Series','Day' ,'Year', 'Month', 'patients']] 
+    df = df[['Series','Day' ,'Year', 'Month', model_name]] 
     # check the head of the dataset
     
     train_pct_index = int(0.9 * len(df))
@@ -115,8 +115,81 @@ start_convert_date = convert_date(dates[0])
 end_convert_date = convert_date(dates[-1])
  
 dates_range=pd.date_range(start=start_convert_date, end=end_convert_date)
-patients=df1[df1.columns[0]].tolist()
-model = load_model("patients_final_model")
-seven_days_prediction_patients = pycaret_patients_seven_days_prediction(patients,dates_range, model)
+
+##########################################################
+patients = df1[df1.columns[0]].tolist()
+seven_days_prediction_patients = save_patients_model(patients, dates_range, "patients") 
+#model = load_model("patients_final_model")
+#seven_days_prediction_patients = pycaret_patients_seven_days_prediction(patients,dates_range, model, "patients")
 print(seven_days_prediction_patients)
+
+totalPatients = df1[df1.columns[1]].tolist()
+seven_days_prediction_totalPatients = save_patients_model(totalPatients, dates_range, "totalPatients")
+#model = load_model("totalPatients_final_model")
+#seven_days_prediction_totalPatients = pycaret_patients_seven_days_prediction(totalPatients,dates_range, model,"totalPatients")
+print(seven_days_prediction_totalPatients)
+
+deaths = df1[df1.columns[2]].tolist()
+seven_days_prediction_deaths = save_patients_model(deaths, dates_range, "deaths")
+#model = load_model("deaths_final_model")
+#seven_days_prediction_deaths = pycaret_patients_seven_days_prediction(deaths,dates_range, model, "deaths")
+print(seven_days_prediction_deaths)
+
+totalDeaths = df1[df1.columns[3]].tolist()
+seven_days_prediction_totalDeaths = save_patients_model(totalDeaths, dates_range, "totalDeaths")
+#model = load_model("totalDeaths_final_model")
+#seven_days_prediction_totalDeaths = pycaret_patients_seven_days_prediction(totalDeaths, dates_range, model, "totalDeaths")
+print(seven_days_prediction_totalDeaths)
+
+recovered = df1[df1.columns[4]].tolist()
+seven_days_prediction_recovered = save_patients_model(recovered, dates_range, "recovered")
+#model = load_model("recovered_final_model")
+#seven_days_prediction_recovered = pycaret_patients_seven_days_prediction(recovered, dates_range, model, "recovered")
+print(seven_days_prediction_recovered)
+
+totalRecovered = df1[df1.columns[5]].tolist()
+seven_days_prediction_totalRecovered = save_patients_model(totalRecovered, dates_range, "totalRecovered")
+#model = load_model("totalRecovered_final_model")
+#seven_days_prediction_totalRecovered = pycaret_patients_seven_days_prediction(totalRecovered, dates_range, model, "totalRecovered")
+print(seven_days_prediction_totalRecovered)
+
+tests = df1[df1.columns[8]].tolist()
+seven_days_prediction_tests = save_patients_model(tests, dates_range, "tests")
+#model = load_model("tests_final_model")
+#seven_days_prediction_totalDeaths = pycaret_patients_seven_days_prediction(totalDeaths, dates_range, model, "totalDeaths")
+print(seven_days_prediction_tests)
+
+totalTests = df1[df1.columns[9]].tolist()
+seven_days_prediction_totalTests = save_patients_model(totalTests, dates_range, "totalTests")
+#model = load_model("totalTests_final_model")
+#seven_days_prediction_totalTests = pycaret_patients_seven_days_prediction(totalTests, dates_range, model, "totalTests")
+print(seven_days_prediction_totalTests)
+
+
+cases = df1[df1.columns[13]].tolist()
+seven_days_prediction_cases = save_patients_model(cases, dates_range, "cases")
+#model = load_model("cases_final_model")
+#seven_days_prediction_cases = pycaret_patients_seven_days_prediction(cases, dates_range, model, "cases")
+print(seven_days_prediction_cases)
+
+
+
+"""
+"patients", 		0
+"totalPatients",    1
+"deaths",    2
+"totalDeaths",  3
+"recovered",   4
+"totalRecovered",  5
+"totalIntubated",  6
+"totalIntensiveCare", 7
+"tests", 8
+"totalTests", 9
+"date", 10
+"critical",  11
+"pneumoniaPercent", 12
+"cases" 13
+
+"""
+
 
